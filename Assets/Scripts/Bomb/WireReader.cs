aeb85e4a78a6b1a6b0986c1f55b0892b89b686e7
Andrew <mrrobottrax@gmail.com>
2024-11-14 19:14:17 -0500

    Serial port

--- Assets/Scripts/Bomb/WireReader.cs
+++ Assets/Scripts/Bomb/WireReader.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO.Ports;
 
 public enum WireColor
 {
@@ -20,17 +21,28 @@ public class WireReader : MonoBehaviour
 	public readonly WireColor[] slots = new WireColor[3];
 	[SerializeField] Image[] debug_wirePreviews;
 
+	private SerialPort data_stream = new("COM3", 9600);
+
 	public bool DisableUpdate;
 
 	void Start()
 	{
 		UpdateDisplay();
+
+		data_stream.Open();
 	}
 
 	void Update()
 	{
 		if (DisableUpdate) return;
 
+		ReadFromArduino();
+		debug_ConnectWires();
+		UpdateDisplay();
+	}
+
+	void debug_ConnectWires()
+	{
 		// Select wire
 		if (Input.GetKeyDown(KeyCode.R))
 		{
@@ -62,6 +74,7 @@ public class WireReader : MonoBehaviour
 		{
 			ConnectWire(debug_grabbedWire, 2);
 		}
+
 	}
 
 	void ConnectWire(WireColor wire, int slotIndex)
@@ -77,8 +90,6 @@ public class WireReader : MonoBehaviour
 
 		// Connect the wire
 		slots[slotIndex] = wire;
-
-		UpdateDisplay();
 	}
 
 	void UpdateDisplay()
@@ -98,5 +109,78 @@ public class WireReader : MonoBehaviour
 			WireColor.Blue => Color.blue,
 			_ => Color.black,
 		};
+	}
+
+	int readingByte = -1;
+	void ReadFromArduino()
+	{
+		if (readingByte != -1)
+		{
+			if (!ReadUntilDone())
+			{
+				return;
+			}
+		}
+
+		// Read until start byte
+		for (int counter = 0; counter <= 100 && data_stream.ReadByte() != 233; ++counter)
+		{
+			if (counter == 99)
+			{
+				Debug.LogError("Not reading properly");
+				return;
+			}
+		}
+
+		readingByte = 0;
+
+		ReadUntilDone();
+	}
+
+	bool ReadUntilDone()
+	{
+		// Read all available bytes
+		for (; readingByte < 3; ++readingByte)
+		{
+			if (data_stream.BytesToRead == 0)
+				break;
+
+			int b = data_stream.ReadByte();
+
+			WireColor wireColor = (WireColor)(readingByte + 1);
+
+			if (b == 255)
+			{
+				// This wire has been disconnected, find which slot it's in
+
+				for (int i = 0; i < slots.Length; ++i)
+				{
+					if (slots[i] == wireColor)
+					{
+						slots[i] = WireColor.None;
+						break;
+					}
+				}
+
+				continue;
+			}
+
+			if (b < 0 || b > 2)
+			{
+				Debug.LogError($"Read {b}");
+				return true;
+			}
+			
+			slots[b] = wireColor;
+		}
+
+		// Done reading
+		if (readingByte == 3)
+		{
+			readingByte = -1;
+			return true;
+		}
+
+		return false;
 	}
 }
